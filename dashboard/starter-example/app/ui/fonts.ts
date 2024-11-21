15a9ea739ea051c3f07157ba0d8a6beba72e2e23
xwhGit <2577468896@qq.com>
2024-11-21 11:12:29 +0800

    init

---
+++ dashboard/starter-example/app/ui/fonts.ts
@@ -0,0 +1,17 @@
+import { Inter, Lusitana, Roboto_Mono } from 'next/font/google'
+ 
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
+export const roboto_mono = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400'
+})
